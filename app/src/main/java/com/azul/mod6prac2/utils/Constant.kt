84b542c845915d603de6f49d9f238e8fc84f6ca2
Azul AF <azulazulada8@gmail.com>
2024-10-06 22:42:21 -0600

    Initial commit

---
+++ app/src/main/java/com/azul/mod6prac2/utils/Constant.kt
@@ -0,0 +1,10 @@
+package com.azul.mod6prac2.utils
+
+object Constant {
+
+    //const val BASE_URL = "https://www.serverbpw.com/"
+    const val BASE_URL = "https://private-a4ba8-dadmtarea.apiary-mock.com/"
+    //const val BASE_URL = "http://10.0.2.2:8888/"
+
+    const val LOGTAG = "APPLOGS"
+}
